f746f47c5b121651fc9b0a3e9c86f8d605b584ec
ImrulEmon <emimrulemon@gmail.com>
2022-03-19 20:49:19 +0600

    title-publish

--- api.js
+++ api.js
@@ -1,24 +1,39 @@
-const loadBook=()=>{
-    const searchText =  document.getElementById('input').value;
-    const url=`https://openlibrary.org/search.json?q=cplus`;
+const loadBook = () => {
+    const searchText = document.getElementById('input').value;
+    const url = `https://openlibrary.org/search.json?q=javascript`;
     fetch(url)
-    .then(res=>res.json())
-    .then(data=>displayBook(data.docs,data.num_found));
+        .then(res => res.json())
+        .then(data => displayBook(data.docs, data.num_found));
 }
 
-const displayBook=(books,number)=>{
-    document.getElementById('book-num').innerHTML=`Total Book : <span class="text-success fw-bold">${number}</span>`;
-    const searchResult=document.getElementById('search-result');
+
+
+const showTitle =(book)=>{
+    const bookTitle = `https://openlibrary.org${book.seed[0]}.json`;
+        fetch(bookTitle)
+            .then(res => res.json())
+            .then(title => document.getElementById('bt').innerText=`${title.subtitle}`);  
+}
+
+const displayBook = (books, number) => {
+    document.getElementById('book-num').innerHTML = `Total Book : <span class="text-success fw-bold">${number}</span>`;
+    const searchResult = document.getElementById('search-result');
     books.forEach(book => {
+
+
+        // book title
+        showTitle(book);
+        // booktitle
+        // const publish = book?.publish_date.length;
         const div = document.createElement('div');
         div.classList.add('col');
-        div.innerHTML=`
+        div.innerHTML = `
         <div class="card h-100">
-        <img class="cover-img img-fluid h-50 w-50" src="https://covers.openlibrary.org/b/id/${book.id_goodreads}-S.jpg" class="card-img-top" alt="...">
+        <img id="cover-img" class="img-fluid mx-auto my-3" src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" alt="...">
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                additional content. This content is a little bit longer.</p>
+            <h5 id="bt" class="card-title"></h5>
+            <p class="card-text">First Publish : </p>
+            <p>${book.author_name}</p>
         </div>
     </div>
         `;
